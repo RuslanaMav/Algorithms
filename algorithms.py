b16e892cbc7a92ad9624df1c144045820260b9fa
RuslanaMav <116889933+RuslanaMav@users.noreply.github.com>
2024-01-10 19:56:03 +0500

    Update and rename binary_search.py to algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,4 @@
+#Бинарный поиск 
 def binary_search(nums, target):
     r, l = 0, len(nums) - 1
     while r <= l:
@@ -6,3 +7,11 @@ def binary_search(nums, target):
         elif nums[m] > target: l = m - 1
         else: r = m + 1
     return -1
+
+#Префиксные суммы
+def prefixSum(arr):
+    pref = [0] * (len(arr))
+    pref[0] = arr[0]
+    for i in range(1, len(arr)):
+        pref[i] = pref[i - 1] + arr[i]
+    return pref
